89a7ce83badd19ed06c6759d5f62721a1977214a
Piotr Bąk <pbonczi7@gmail.com>
2020-11-26 18:30:23 +0100

    Formatting

--- src/main.c
+++ src/main.c
@@ -21,24 +21,24 @@ static int compare_value(int * ptr, int * value);
 
 int main(void)
 {
-	int table[SAMPLE_SIZE];
-	int value;
-	int success;
+    int table[SAMPLE_SIZE];
+    int value;
+    int success;
 
-	success = copy_to_table(table);
+    success = copy_to_table(table);
 
-	if ( success )
-	{
-		success = compare_value(table, &value);
-	}
+    if ( success )
+    {
+        success = compare_value(table, &value);
+    }
 
     if (success )
     {
-    	printf("Searched value is: 0x%x\n",value);
+        printf("Searched value is: 0x%x\n",value);
     }
     else
     {
-    	printf("Cannot find searched value\n");
+        printf("Cannot find searched value\n");
     }
 
     return 0;
@@ -48,64 +48,64 @@ int main(void)
 
 static int copy_to_table(int * ptr)
 {
-	int success = 0u;
+    int success = 0u;
 
-	if (ptr != NULL)
-	{
-		for (int i = 0u; i < SAMPLE_SIZE; i++)
-		{
-			ptr[i] = GetNext();
-		}
-		success = 1u;
-	}
-	else
-	{
-		printf("Invalid address of table");
-	}
+    if (ptr != NULL)
+    {
+        for (int i = 0u; i < SAMPLE_SIZE; i++)
+        {
+            ptr[i] = GetNext();
+        }
+        success = 1u;
+    }
+    else
+    {
+        printf("Invalid address of table");
+    }
 
-	return success;
+    return success;
 }
 
 
 static int compare_value(int * ptr, int * value)
 {
-	int temp;
-	int even_value = 0u;
-	int success = 0;
+    int temp;
+    int even_value = 0u;
+    int success = 0;
 
-	if ( (ptr != NULL)
-		&& (value != NULL) )
+    if ( (ptr != NULL)
+        && (value != NULL) )
     {
-		for (int i = 0u; (i < SAMPLE_SIZE) && (success==0u); i++)
-		{
-			temp = ptr[i];
+        for (int i = 0u; (i < SAMPLE_SIZE) && (success==0u); i++)
+        {
+            temp = ptr[i];
 
-			for (int j=0u; (j <  SAMPLE_SIZE) && (even_value==0u); j++)
-			{
-				if (i != j)
-				{
-					if ( temp == ptr[j] )
-					{
-						even_value = 1u;
-					}
-				}
-			}
+            for (int j=0u; (j <  SAMPLE_SIZE) && (even_value==0u); j++)
+            {
+                if (i != j)
+                {
+                    if ( temp == ptr[j] )
+                    {
+                        even_value = 1u;
+                    }
+                }
+            }
 
-			if (even_value == 0u)
-			{
-				success = 1u;
-				*value = temp;
-			}
-			else
-			{
-				even_value = 0u;
-			}
-		}
+            if (even_value == 0u)
+            {
+                success = 1u;
+                *value = temp;
+            }
+            else
+            {
+                even_value = 0u;
+            }
+        }
     }
-	else
-	{
-		printf("Invalid address of table or return value");
-	}
+    else
+    {
+        printf("Invalid address of table or return value");
+    }
 
-	return success;
+    return success;
 }
