eea3c15d1390481bd59ae5f55e479e7b4895ef7e
Piotr Bąk <pbonczi7@gmail.com>
2020-11-26 18:24:04 +0100

    Add static functions

--- src/main.c
+++ src/main.c
@@ -16,46 +16,23 @@ GetNext() {
       return -1;
 }
 
+static int copy_to_table(int * ptr);
+static int compare_value(int * ptr, int * value);
+
 int main(void)
 {
 	int table[SAMPLE_SIZE];
 	int value;
-	int temp;
-	int even_value = 0u;
-	int success = 0;
+	int success;
 
-	for (int i = 0u; i < SAMPLE_SIZE; i++)
+	success = copy_to_table(table);
+
+	if ( success )
 	{
-		table[i] = GetNext();
+		success = compare_value(table, &value);
 	}
 
-	for (int i = 0u; (i < SAMPLE_SIZE) && (success==0u); i++)
-	{
-		temp = table[i];
-
-		for (int j=0u; (j <  SAMPLE_SIZE) && (even_value==0u); j++)
-		{
-			if (i != j)
-			{
-				if ( temp == table[j] )
-				{
-					even_value = 1u;
-				}
-			}
-		}
-
-		if (even_value == 0u)
-		{
-			success = 1u;
-			value = temp;
-		}
-		else
-		{
-			even_value = 0u;
-		}
-	}
-
-    if (success == 1u)
+    if (success )
     {
     	printf("Searched value is: 0x%x\n",value);
     }
@@ -64,9 +41,71 @@ int main(void)
     	printf("Cannot find searched value\n");
     }
 
-
-
     return 0;
 
+}
 
+
+static int copy_to_table(int * ptr)
+{
+	int success = 0u;
+
+	if (ptr != NULL)
+	{
+		for (int i = 0u; i < SAMPLE_SIZE; i++)
+		{
+			ptr[i] = GetNext();
+		}
+		success = 1u;
+	}
+	else
+	{
+		printf("Invalid address of table");
+	}
+
+	return success;
+}
+
+
+static int compare_value(int * ptr, int * value)
+{
+	int temp;
+	int even_value = 0u;
+	int success = 0;
+
+	if ( (ptr != NULL)
+		&& (value != NULL) )
+    {
+		for (int i = 0u; (i < SAMPLE_SIZE) && (success==0u); i++)
+		{
+			temp = ptr[i];
+
+			for (int j=0u; (j <  SAMPLE_SIZE) && (even_value==0u); j++)
+			{
+				if (i != j)
+				{
+					if ( temp == ptr[j] )
+					{
+						even_value = 1u;
+					}
+				}
+			}
+
+			if (even_value == 0u)
+			{
+				success = 1u;
+				*value = temp;
+			}
+			else
+			{
+				even_value = 0u;
+			}
+		}
+    }
+	else
+	{
+		printf("Invalid address of table or return value");
+	}
+
+	return success;
 }
